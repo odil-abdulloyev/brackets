3036a1892c83c2a892918754e5bbd5879697094e
Odil Abdulloyev <abdulloyevodil@mail.ru>
2021-01-23 22:23:14 +0500

    refactor: update solution

--- src/index.js
+++ src/index.js
@@ -1,43 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  if (str === '()') {
-    return true;
-  } else if (str === '((()))()') {
-    return true;
-  } if (str === '())(') {
-    return false;
-  } else if (str === '([{}])') {
-    return true;
-  } else if (str === '[(])') {
-    return false;
-  } else if (str === '[]()') {
-    return true;
-  } else if (str === '[]()(') {
-    return false;
-  } else if (str === '||') {
-    return true;
-  } else if (str === '|()|') {
-    return true;
-  } else if (str === '|(|)') {
-    return false;
-  } else if (str === '|()|(||)||') {
-    return true;
-  } else if (str === '111115611111111222288888822225577877778775555666677777777776622222') {
-    return true;
-  } else if (str === '5555512575557777777555566667888888667661133833448441111222233333444442266666') {
-    return false;
-  } else if (str === '8888877878887777777888888887777777887887788788887887777777788888888887788888') {
-    return false;
-  } else if (str === '111115611111111156111111112222888888222255778777787755556666777777777766222221111222288888822225577877778775555666677777777776622222') {
-    return true;
-  } else if (str === '[]][[]') {
-    return false;
-  } else if (str === '[]][[]') {
-    return false;
-  } else if (str === '([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]]))()') {
-    return false;
-  } else if (str === '([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])(())') {
-    return true;
-  } else if (str === '([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])((([[[[(({{{}}}(([](((((((())))||||||))))[[{{|{{}}|}}[[[[]]]]{{{{{}}}}}]]))))]]]])))') {
-    return true;
-  }
+  const brackets = {};
+  bracketsConfig.forEach(([open, close]) => {
+    brackets[open] = close;
+  });
+  const stack = [];
+  str.split('').forEach((el) => {
+    const top = stack[stack.length - 1];
+    if (el === brackets[top]) {
+      stack.pop();
+    } else {
+      stack.push(el);
+    }
+  });
+  return stack.length === 0;
 }
